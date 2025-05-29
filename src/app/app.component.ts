6a37303850948b889a83f6ffeed97ed226978ce9
KasturiRajini <kasturirajini87@gmail.com>
2025-05-29 11:40:41 +0530

    angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Form, FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'Rajini';
+  message = "";
+ 
+  userentered = "";
+  
+  onClick(){
+    
+    console.log("Welcome "+this.userentered);
+  }
+  
 }
