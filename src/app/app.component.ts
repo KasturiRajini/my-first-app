aad7e169e2c6bf4480e0ccd37f46137224d56f57
KasturiRajini <kasturirajini87@gmail.com>
2025-06-07 15:22:17 +0530

    practiced!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Form, FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,5 +20,7 @@ export class AppComponent {
     
     console.log("Welcome "+this.userentered);
   }
+  public colors = ["red","blue","orange","black","white"];
+  public pickedColor = "black";
   
 }
